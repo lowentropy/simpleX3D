e1a7a60427b3fde86ef279e2d158a7376fcbbd60
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2009-09-18 04:11:32 -0500

    put generated m4 macros into their own dir

--- clean.sh
+++ clean.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 make distclean
-rm -f Makefile.in configure
+rm -f Makefile.in aclocal.m4 configure
 rm -f src/Makefile.in
-rm -rf build-aux autom4te.cache
-rm -rf aclocal.m4 config.log config.status libtool
+rm -rf build-aux autom4te.cache m4
