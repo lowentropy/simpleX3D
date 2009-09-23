9465bd136e717b204897a1db28092cbfed868e3b
Nathan Matthews <lowentropy@gmail.com>
2009-09-23 01:31:18 -0500

    provided comments for every element generated by macros

--- src/macros.h
+++ src/macros.h
@@ -1,25 +1,31 @@
 #ifndef _X3D_MACROS_H_
 #define _X3D_MACROS_H_
 
-#define X3D_NODE(name) \
+#define X3D_BEGIN_NODE(name) \
 	private: \
+		/**
+		   Runtime definition of node type,
+		   containing its name and fields.
+		  */ \
 		X3D::NodeDefinition* definition; \
 	public: \
-	/**
-	 * Static initializer for name.
-	 * 
-	 * Registers the name node type with X3D, and sets the
-	 * #definition variable.
-	 */ \
-	static void init() { \
-		definition = new X3D::NodeDefinition(#name);
+		/**
+		   Static initializer for name.
+		   
+		   Registers the name node type with X3D, and sets the
+		   #definition variable.
+		 */ \
+		static void init() { \
+			definition = new X3D::NodeDefinition(#name);
 
 #define X3D_FIELD_IN(name) definition->addInputField(#name, &##name);
 #define X3D_FIELD_VAR(name) definition->addVariableField(#name, &get_##name);
 #define X3D_FIELD_OUT(name) definition->addOutputField(#name, &##name);
 
+#define X3D_END_NODE }
+
 #define X3D_INIT(type, name) \
-	/** Initialize-only field. */ \
+	/** Initialize-only type field. */ \
 	public: type name;
 
 #define X3D_IN(type, name) \
@@ -28,7 +34,7 @@
 		   Default virtual action to take on the
 		   name input event.
 
-		   @param value value received with event
+		   @param value type value received with event
 		 */ \
 		virtual void on_##name(type value) {} \
 	public: \
@@ -37,7 +43,7 @@
 		   The body of this function should call the virtual
 		   function #on_##name.
 
-		   @param value value received with this event
+		   @param value type value received with this event
 		 */ \
 		void name(type value)
 
@@ -48,7 +54,7 @@
 		   name input event. Implementors MUST
 		   provide this function.
 
-		   @param value value received with event
+		   @param value type value received with event
 		 */ \
 		virtual on_##name(type, value) = 0; \
 	public: \
@@ -57,7 +63,7 @@
 		   The body of this function should call the abstract
 		   function #on_##name.
 
-		   @param value value received with event
+		   @param value type value received with event
 		 */ \
 		void name(type value)
 
@@ -70,7 +76,7 @@
 		   and then queues an event for any routes connected to this
 		   field.
 
-		   @param value value saved and sent with event
+		   @param value type value saved and sent with event
 		 */ \
 		void name(type value) { \
 			on_##name(value); \
@@ -81,9 +87,8 @@
 		   Default virtual action to take on the
 		   name output event.
            
-		   @param value new value of name
+		   @param value new type value of name
 		 */ \
-		virtual void on_##name(type value) {} \
 		virtual void on_##name(type value) {}
 
 #define X3D_OUT_ABSTRACT(type, name) \
@@ -95,7 +100,7 @@
 		   and then queues an event for any routes connected to this
 		   field.
 
-		   @param value value saved and sent with event
+		   @param value type value saved and sent with event
 		 */ \
 		void name(type value) { \
 			on_##name(value); \
@@ -103,49 +108,181 @@
 			output(#name, value); } \
 	protected: \
 		 /**
-		  * Abstract action to take on the
-		  * name output event. Implementors MUST
-		  * provide this function.
-		  *
-		  * @param value new value of name
+		    Abstract action to take on the
+		    name output event. Implementors MUST
+		    provide this function.
+		  
+		    @param value new type value of name
 		  */ \
 		virtual void on_##name(type value) = 0;
 
 #define X3D_OUT_CUSTOM(type, name) \
 	protected: \
+		/**
+		   Default virtual action to take on the
+		   name output event.
+           
+		   @param value new type value of name
+		 */ \
 		virtual void on_##name(type value) {} \
-	public: void name(type value)
+	public: \
+		/**
+		   Trigger the output event with the given value.
+		   This function should call the virtual function
+		   #on_##name with the new value BEFORE setting the
+		   output variable, and then queue an event for any
+		   routes connectd to this field.
+
+		   @param value type value saved and sent with event
+		 */ \
+		void name(type value)
 
 #define X3D_OUT_CUSTOM_ABSTRACT(type, name) \
-	protected: virtual void on_##name(type value) = 0; \
-	public: void name(type value)
+	protected: \
+		 /**
+		    Abstract action to take on the
+		    name output event. Implementors MUST
+		    provide this function.
+		  
+		    @param value new type value of name
+		  */ \
+		virtual void on_##name(type value) = 0; \
+	public: \
+		/**
+		   Trigger the output event with the given value.
+		   This function should call the abstract function
+		   #on_##name with the new value BEFORE setting the
+		   output variable, and then queue an event for any
+		   routes connectd to this field.
+
+		   @param value type value saved and sent with event
+		 */ \
+		void name(type value)
 
 #define X3D_INOUT(type, name) \
+	/** Holds the last value sent to name##_changed() */ \
 	public: type name; \
-	public: void set_##name(type value) { name##_changed(value); } \
-	public: void name##_changed(type value) { \
-		on_##name##_changed(value); \
-		name = value; output(#name, value); } \
-	protected: virtual void on_##name##_changed(type value) {}
+	public: \
+		/**
+		   Simply calls name##_changed() with the input value.
+		   
+		   @param value type value received with event
+		 */ \
+		void set_##name(type value) { name##_changed(value); } \
+	public: \
+		/**
+		   Trigger the output event with the given value.
+		   This function automatically calls the virtual function
+		   #on_##name##_changed with the new value, sets the output variable,
+		   and then queues an event for any routes connected to this
+		   field.
+
+		   @param value type value saved and sent with event
+		 */ \
+		void name##_changed(type value) { \
+			on_##name##_changed(value); \
+			name = value; output(#name, value); } \
+	protected: \
+		/**
+		   Default virtual action to take on the
+		   name output event.
+           
+		   @param value new type value of name
+		 */ \
+		virtual void on_##name##_changed(type value) {}
 
 #define X3D_INOUT_ABSTRACT(type, name) \
+	/** Holds the last value sent to name##_changed() */ \
 	public: type name; \
-	public: void set_##name(type value) { name##_changed(value); } \
-	public: void name##_changed(type value) { \
-		on_##name##_changed(value); \
-		name = value; output(#name, value); } \
-	protected: virtual void on_##name##_changed(type value) = 0;
+	public: \
+		/**
+		   Simply calls name##_changed() with the input value.
+		   
+		   @param value type value received with event
+		 */ \
+		void set_##name(type value) { name##_changed(value); } \
+	public: \
+		/**
+		   Trigger the output event with the given value.
+		   This function automatically calls the abstract function
+		   #on_##name##_changed with the new value, sets the output variable,
+		   and then queues an event for any routes connected to this
+		   field.
+
+		   @param value type value saved and sent with event
+		 */ \
+		void name##_changed(type value) { \
+			on_##name##_changed(value); \
+			name = value; output(#name, value); } \
+	protected: \
+		 /**
+		    Abstract action to take on the
+		    name output event. Implementors MUST
+		    provide this function.
+		  
+		    @param value new type value of name
+		  */ \
+		virtual void on_##name##_changed(type value) = 0;
 
 #define X3D_INOUT_CUSTOM(type, name) \
+	/** Holds the last value sent to name##_changed() */ \
 	public: type name; \
-	public: void set_##name(type value) { name##_changed(value); } \
-	public: void name##_changed(type value) \
-	protected: virtual void on_##name##_changed(type value) {}
+	public: \
+		/**
+		   Simply calls name##_changed() with the input value.
+		   
+		   @param value type value received with event
+		 */ \
+		void set_##name(type value) { name##_changed(value); } \
+	protected: \
+		/**
+		   Default virtual action to take on the
+		   name output event.
+           
+		   @param value new type value of name
+		 */ \
+		virtual void on_##name##_changed(type value) {} \
+	public: \
+		/**
+		   Trigger the output event with the given value.
+		   This function should call the virtual function
+		   #on_##name##_changed with the new value BEFORE setting the
+		   output variable, and then queue an event for any
+		   routes connectd to this field.
+
+		   @param value type value saved and sent with event
+		 */ \
+		void name##_changed(type value)
 
 #define X3D_INOUT_CUSTOM_ABSTRACT(type, name) \
+	/** Holds the last value sent to name##_changed() */ \
 	public: type name; \
-	public: void set_##name(type value) { name##_changed(value); } \
-	public: void name##_changed(type value) \
-	protected: virtual void on_##name##_changed(type value) = 0;
+	public: \
+		/**
+		   Simply calls name##_changed() with the input value.
+		   
+		   @param value type value received with event
+		 */ \
+		void set_##name(type value) { name##_changed(value); } \
+	protected: \
+		 /**
+		    Abstract action to take on the
+		    name output event. Implementors MUST
+		    provide this function.
+		   
+		    @param value new type value of name
+		  */ \
+		virtual void on_##name##_changed(type value) = 0; \
+	public: \
+		/**
+		   Trigger the output event with the given value.
+		   This function should call the abstract function
+		   #on_##name##_changed with the new value BEFORE setting the
+		   output variable, and then queue an event for any
+		   routes connectd to this field.
+
+		   @param value type value saved and sent with event
+		 */ \
+		void name##_changed(type value)
 
 #endif // #ifndef _X3D_MACROS_H_
