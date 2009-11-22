ab3c4861e9eeca872970cb133c1f3ff83e6db369
Nathan Matthews <lowentropy@gmail.com>
2009-11-22 16:25:37 -0600

    better xml load test

--- test/internal/XmlLoadTests.h
+++ test/internal/XmlLoadTests.h
@@ -8,4 +8,13 @@ TEST(XmlLoad, LoadedSceneShouldHaveCorrectStructure) {
     ASSERT_THAT(b, NotNull());
     EXPECT_EQ(SFTime(3), a->getField("cycleInterval")->get());
     EXPECT_EQ(SFTime(5), b->getField("cycleInterval")->get());
+    EXPECT_EQ(SFBool(true), a->getField("loop")->get());
+    EXPECT_EQ(SFBool(false), b->getField("loop")->get());
+    const list<Route*>& routes = a->getField("time")->getOutgoingRoutes();
+    EXPECT_EQ(1, routes.size());
+    const Route* route = routes.front();
+    EXPECT_EQ(a, route->fromField->getNode());
+    EXPECT_EQ(b, route->toField->getNode());
+    EXPECT_EQ(a->getField("time"), route->fromField);
+    EXPECT_EQ(b->getField("pauseTime"), route->toField);
 }
